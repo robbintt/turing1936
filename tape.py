7add3499f38397bb791dec6e884ed01b988aaf4d
Trent Robbins <robbintt@gmail.com>
2015-06-09 01:57:35 -0700

    Added many basic instructions and most of the framework.

--- tape.py
+++ tape.py
@@ -1,8 +1,7 @@
 """
-
-
-
 """
+import .instructions
+
 
 class turing_machine(object):
     """
@@ -46,11 +45,91 @@ class turing_machine(object):
     1. It could be a list with an index stored as the 'scanned square'
     2. It could be a variable which is popped off left and onto right. This approach requires maintaining two strings, one of which increases to the left, one of which increases to the right.
     3. 
+
+    Turing's Configuration specifies a sequential list of instructions and the next m-configuration.
+
+    How should Configuration instructions be managed?
+    1. Configuration is a sequential list of actions finally providing the next state.
+    2. It follows that state is a 'named sequence' best represented by a key-value pair.
+    3. The key is the name of the m-configuration.
+    4. The value is a tuple, V. V[0] is a list of instructions, V[1] is name of next m-configuration.
+    5. V[0] is a list of keys (instructions) whose values are functions in a lookup table (dictionary).
     """
 
 
     def __init__(self):
-        self.tape = ""
+        """
+        This class is a general representation of a turing machine.
+        m-configuration and operations must be defined outside this class.
+        """
+        self.m_config = ""
+        self.operations = {}
+
+        # One method of managing the tape is to individually manage the left and right
+        # sides of the tape. The ordinality of the tape is defined as such:
+        # tape[i] can be popped or appended. The last item is always the closest to the scanner.
+        # This is a complete description because the tape has only one reference. The scanner.
+        self.left_tape = list("")
+        self.right_tape = list("")
+        self.scanned_symbol = ""
+
+
+    def scan_to_right():
+        """
+        """
+        # Store current scanned symbol on the other tape.
+        self.left_tape.append(self.scanned_symbol)
         
+        try:
+            self.scanned_symbol = self.right_tape.pop()
+        except IndexError:
+            # If the list is empty, generate an empty space.
+            self.scanned_symbol = ""
+
+
+    def scan_to_left():
+        """
+        """
+        # Store current scanned symbol on the other tape.
+        self.right_tape.append(self.scanned_symbol)
+        
+        try:
+            self.scanned_symbol = self.left_tape.pop()
+        except IndexError:
+            # If the list is empty, generate an empty space.
+            self.scanned_symbol = ""
+
+
+    def ingest_behavior(configuration):
+        """ Process the current configuration's operations.
+        """
+        operations = configuration[0]
+        next_mconfiguration = configuration[1]
+
+        # Chew up the string
+        while len(operations):
+            operations.pop(0) # inefficient, O(n), fix later
+            # Apply the function corresponding to the operation
+            apply_operation[operations[0]]()
+
+
+# Make a machine
+turing_machine_0 = turing_machine()
+
+# This doesn't take into account scanned symbol yet.
+# With scanned symbol, the table takes on more complexity.
+turingmachine_0.operations = {
+        'b' : (['P0','R'],'c'),
+        'c' : ('R','e'), 
+        'e' : (['P1','R'],'f'),
+        'f' : ('R','b')
+        }
+
+turingmachine_0.instructions = {
+        'P0' : , .instructions.print_zero()
+        'P1' : , .instructions.print_one()
+        'R' : , self.scan_to_right()
+        'L' : , self.scan_to_left()
+        }
 
 
